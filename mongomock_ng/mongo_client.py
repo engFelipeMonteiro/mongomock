0e2fed5a59ccbad41b18e2910c60be4e527bc95c
Eng. Felipe Monteiro <fmj1988@gmail.com>
2026-05-24 09:06:00 -0300

    chore: sync aggregate.py, mongo_client.py with origin/develop

--- mongomock_ng/mongo_client.py
+++ mongomock_ng/mongo_client.py
@@ -158,7 +158,7 @@ class MongoClient:
                 _db.drop_collection(col_name)
 
         if isinstance(name_or_db, Database):
-            db = next(db for db in self._database_accesses.values() if db is name_or_db)
+            db = next((db for db in self._database_accesses.values() if db is name_or_db), None)
             if db is not None:
                 drop_collections_for_db(db)
 
